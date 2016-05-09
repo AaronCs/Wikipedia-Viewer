b14cfe2350a543925a3c2aae01c4e364ca5a2d88
aaroncs <aaronsy2007@yahoo.com>
2016-05-08 23:51:55 -0700

    Added toggle event of search button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,22 @@
-$(function(
-	console.log("hello")
-;));
+$(function(){
+	$('.search-box').hide();
+	// $('.big-search-button').hide();
+
+	$('.big-search-button').on('click', function(){
+		$('.big-search-button').slideToggle(500, function(){
+			$('.search-box').slideToggle(600);
+		});
+	});
+
+	$('body').on('click', function(evt){
+		console.log(evt.target.class);
+		if (evt.target.class == ".search-box"){
+			return;
+		}
+		else {
+			$('.search-box').slideToggle(500, function(){
+				$('.big-search-button').slideToggle(600);
+			})
+		};
+	});
+});
