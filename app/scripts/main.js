04c7b7d8ae1c8b96c9790dc1e787f555550518d6
aaroncs <aaronsy2007@yahoo.com>
2016-05-10 18:50:53 -0700

    Changed search box styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,27 @@
 $(function(){
+	var searchBox = $('.searchbox');
+	var searchButton2 = $('.search-button2');
 	// On the fence about this
 	// 'This' as in the switching of searchbox icons.
+	searchButton2.toggleClass('fa-times');
 	$(".search").on('focus', function(){
-		if (!$(".search-button").hasClass('fa-times')){
-			$(".search-button").toggleClass('fa-times');
+		if (!searchButton2.hasClass('fa-times')){
+			searchButton2.toggleClass('fa-times');
 		}
 	});
 
 	$(".search").blur(function(){
-		if($(".search-button").hasClass('fa-times')){
-			$(".search-button").toggleClass('fa-times');
+		if(searchButton2.hasClass('fa-times')){
+			searchButton2.toggleClass('fa-times');
 		}
 	});
 
 	// Include searchbox fa-times click --> reset searchbox
+
+	$('.search-button').on('click', function(){
+		if($(".search-button").hasClass('fa-times')){
+			console.log("Hello");
+			$('.search:text').val('Hello');
+		}
+	});
 });
